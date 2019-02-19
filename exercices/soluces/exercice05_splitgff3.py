0071c3f9e8e46ad69894a7fe66c83f35ba7091d2
sderozier <sandra.derozier@inra.fr>
2019-02-19 22:39:06 +0100

    Update exercice05_splitgff3.py

--- exercices/soluces/exercice05_splitgff3.py
+++ exercices/soluces/exercice05_splitgff3.py
@@ -14,18 +14,18 @@ with open("B_subtilis_GeneOnly.txt", "w") as filout:
                 champs = line.split("\t")
                 # Si la ligne correspond à un gene
                 if champs[2] == "gene":
-                    start =champs[3]
+                    start = champs[3]
                     stop = champs[4]
                     # Split de la dernière colonne
                     attributs = champs[8].split(";")
                     # Parcours des différents attributs de la dernière colonne
                     for attribut in attributs:
-                        # Solution 1 (avec un split)
+                        # Split sur le caractère "="
                         couple = attribut.split("=")
                         if couple[0] == "gene":
                             gene = couple[1]
                         elif couple[0] == "locus_tag":
                             locustag = couple[1]
-                            
+                    
                     # Ecriture dans le fichier de sortie
                     filout.write("{:<12}{:<15}{:<10}{:<10}\n".format(gene,locustag,start,stop))
