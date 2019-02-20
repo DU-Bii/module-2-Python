296de79caddc4b7ac2625be952783111a809437d
patrickfuchs <patrick.fuchs@univ-paris-diderot>
2019-02-20 09:05:33 +0100

    Corrections exo 05

--- exercices/soluces/exercice05_splitgff3.py
+++ exercices/soluces/exercice05_splitgff3.py
@@ -1,31 +1,31 @@
+# Ouverture en lecture du fichier GFF3 initial
 # Ouverture en écriture du fichier tabulé final
-with open("B_subtilis_GeneOnly.txt", "w") as filout:
+with open("Bacillus_subtilis.gff3", "r") as filin, \
+     open("B_subtilis_GeneOnly.txt", "w") as filout:
     # Ecriture de l'en-tête du fichier de sortie
-    filout.write("{:<12}{:<15}{:<10}{:<10}\n".format("Name","Locus-tag","Start","Stop"))
-    # Ouverture en lecture du fichier GFF3 initial
-    with open("Bacillus_subtilis.gff3", "r") as filin:
-        # Parcours du contenu du fichier ligne par ligne
-        for line in filin:
-            # Retrait du saut de ligne
-            line = line.strip()
-            # Si la ligne ne commence pas par un #
-            if line[0] != "#":
-                # Split sur les tabulations
-                champs = line.split("\t")
-                # Si la ligne correspond à un gene
-                if champs[2] == "gene":
-                    start = champs[3]
-                    stop = champs[4]
-                    # Split de la dernière colonne
-                    attributs = champs[8].split(";")
-                    # Parcours des différents attributs de la dernière colonne
-                    for attribut in attributs:
-                        # Split sur le caractère "="
-                        couple = attribut.split("=")
-                        if couple[0] == "gene":
-                            gene = couple[1]
-                        elif couple[0] == "locus_tag":
-                            locustag = couple[1]
-                    
-                    # Ecriture dans le fichier de sortie
-                    filout.write("{:<12}{:<15}{:<10}{:<10}\n".format(gene,locustag,start,stop))
+    filout.write("{:<12}{:<15}{:<10}{:<10}\n".format("Name","Locus-tag",
+                                                     "Start","Stop"))
+    # Parcours du contenu du fichier ligne par ligne
+    for line in filin:
+        # Si la ligne ne commence pas par un #
+        if not(line.startswith("#")):
+            # On nettoie le retour à la ligne avec strip() puis
+            # split() sur les tabulations
+            champs = line.strip().split("\t")
+            # Si la ligne correspond à un gene
+            if champs[2] == "gene":
+                start = int(champs[3])
+                stop = int(champs[4])
+                # split() de la dernière colonne sur le point-virgule
+                attributs = champs[8].split(";")
+                # Parcours des différents attributs de la dernière colonne
+                for attribut in attributs:
+                    # Split sur le caractère "="
+                    couple = attribut.split("=")
+                    if couple[0] == "gene":
+                        gene = couple[1]
+                    elif couple[0] == "locus_tag":
+                        locustag = couple[1]
+                # Ecriture dans le fichier de sortie
+                filout.write("{:<12s}{:<15s}{:<10d}{:<10d}\n"
+                             .format(gene, locustag, start, stop))
